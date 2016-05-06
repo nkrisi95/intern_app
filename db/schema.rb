8fd73163020b66c665ff36c9cbfcfe59eaf958b8
Kristóf Nagy <cura95@gmail.com>
2016-05-06 14:23:00 +0200

    Added migration 'plan to users'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506121435) do
+ActiveRecord::Schema.define(version: 20160506122142) do
 
   create_table "plans", force: true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160506121435) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
