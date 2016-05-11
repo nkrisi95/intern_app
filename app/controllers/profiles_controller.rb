d5bdd2d40b700f194839daf53c750e94f83213e4
Kristóf Nagy <cura95@gmail.com>
2016-05-11 17:10:15 +0200

    Added profiles new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-  def index    
+  def index
   end
 
   def show
@@ -8,6 +8,22 @@ class ProfilesController < ApplicationController
   def create
   end
 
+  def new
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile
+  end
+
+  def update
+  end
+
   def edit
+  end
+
+  private
+
+  def user_params
+  end
+
+  def find_user_params
   end
 end
